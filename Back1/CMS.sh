3438cc0177faee0fdc6b4ddf2cb887a710b3eac7
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-25 23:45:32 +0300

    Update CMS.sh

--- Back1/CMS.sh
+++ Back1/CMS.sh
@@ -1,25 +1,35 @@
-###устанавливаем php для CMS
+# Устанавливаем PHP и необходимые модули
 sudo apt install php libapache2-mod-php php-mysql php-curl php-gd php-json php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y;
 
-### скачать wordpress, распаковать
+# Скачиваем WordPress и распаковываем
 sudo wget https://ru.wordpress.org/wordpress-6.7.1-ru_RU.tar.gz;
 sudo tar -xzvf wordpress-6.7.1-ru_RU.tar.gz;
 
-### копируем все в папку apache
+# Копируем все в папку apache
 sudo cp -r wordpress/* /var/www/html/;
 
-### создаем базу для wp
+# Создаем базу для WordPress
 mysql -u root -e "CREATE DATABASE WP;";
 
-### раздаем права создаем пользователя
+# Создаем пользователя и даем права
 mysql -u root -e "CREATE USER 'wp'@'%' IDENTIFIED BY 'password';";
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'wp'@'%';";
 
-### качаем wp-config
+# Скачиваем wp-config
 sudo wget https://raw.githubusercontent.com/Vladimir-Otus/work/refs/heads/main/Back1/wp-config.php
 
-### копируем его в папку wordpress
+# Удаляем старый wp-config.php если существует
+if [ -f "/var/www/html/wp-config.php" ]; then
+    sudo rm /var/www/html/wp-config.php;
+fi
+
+# Копируем новый wp-config.php в папку WordPress
 sudo cp wp-config.php /var/www/html/;
 
-### удаляем файл apache index (чтобы резолвился нормальный wordpress)
-sudo rm /var/www/html/index.html;
+# Удаляем временный файл wp-config.php
+sudo rm wp-config.php;
+
+# Удаляем файл apache index (чтобы резолвился нормальный WordPress)
+if [ -f "/var/www/html/index.html" ]; then
+    sudo rm /var/www/html/index.html;
+fi
