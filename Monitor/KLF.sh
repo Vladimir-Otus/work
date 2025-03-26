c9742f684d4cfb208fc8a701efbe736d552f64f4
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-26 13:43:43 +0300

    Update KLF.sh

--- Monitor/KLF.sh
+++ Monitor/KLF.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Установка компонентов (предполагаем, что .deb файлы уже скачаны)
-sudo dpkg -i kibana.deb logstash.deb filebeat.deb
+sudo dpkg -i /home/vt/filebeat_8.9.1_amd64-224190-bc3f59.deb
+sudo dpkg -i /home/vt/kibana_8.9.1_amd64-224190-f7ebba.deb
+sudo dpkg -i /home/vt/logstash_8.9.1_amd64-224190-11b1b0.deb
 sudo apt-get install -f -y
+
 
 # Запуск сервисов
 sudo systemctl daemon-reload
