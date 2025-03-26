dbfe6d161e5f7a09728e9d1592113c065177f475
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-26 17:46:20 +0300

    Update elastic.sh

--- Monitor/elastic.sh
+++ Monitor/elastic.sh
@@ -2,10 +2,32 @@
 
 # Установка зависимостей
 sudo apt update
-sudo apt install -y default-jdk
+sudo apt install -y default-jdk wget
 
 # Установка Elasticsearch (предполагаем, что .deb файл уже скачан)
 sudo dpkg -i /home/vt/elasticsearch_8.9.1_amd64-224190-f79e75.deb
+
+# Скачивание и замена конфигурационного файла
+ELASTIC_YML="/etc/elasticsearch/elasticsearch.yml"
+BACKUP_YML="/etc/elasticsearch/elasticsearch.yml.bak"
+
+# Создаем бекап существующего файла, если он есть
+if [ -f "$ELASTIC_YML" ]; then
+    sudo cp "$ELASTIC_YML" "$BACKUP_YML"
+    echo "Существующий конфигурационный файл сохранен как $BACKUP_YML"
+    sudo rm "$ELASTIC_YML"
+fi
+
+# Скачиваем новый конфигурационный файл
+sudo wget -O "$ELASTIC_YML" https://raw.githubusercontent.com/Vladimir-Otus/work/refs/heads/main/Monitor/elasticsearch.yml
+
+# Проверяем успешность скачивания
+if [ $? -eq 0 ]; then
+    echo "Конфигурационный файл успешно загружен и сохранен в $ELASTIC_YML"
+else
+    echo "Ошибка при загрузке конфигурационного файла"
+    exit 1
+fi
 
 # Простая настройка памяти
 sudo mkdir -p /etc/elasticsearch/jvm.options.d
