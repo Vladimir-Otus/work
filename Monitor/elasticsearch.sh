987a1411d7b61a4db2bc8e5d5b572d89ef8b80a3
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-26 13:24:14 +0300

    Update elasticsearch.sh

--- Monitor/elasticsearch.sh
+++ Monitor/elasticsearch.sh
@@ -5,10 +5,10 @@ sudo apt update
 sudo apt install -y default-jdk
 
 # Установка Elasticsearch (предполагаем, что .deb файл уже скачан)
-sudo dpkg -i elasticsearch_8.9.1_amd64-224190-f79e75.deb
-sudo apt-get install -f -y
+sudo dpkg -i /home/vt/elasticsearch_8.9.1_amd64-224190-f79e75.deb
 
 # Простая настройка памяти
+sudo mkdir -p /etc/elasticsearch/jvm.options.d
 echo "-Xms1g" | sudo tee /etc/elasticsearch/jvm.options.d/jvm.options
 echo "-Xmx1g" | sudo tee -a /etc/elasticsearch/jvm.options.d/jvm.options
 
