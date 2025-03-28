3a3716cd4f4ee66e67eb2ba212ef628d362c3575
Vladimir-Otus <v.v.tikhonov@yandex.ru>
2025-03-28 15:07:01 +0300

    Update Front.sh

--- Front/Front.sh
+++ Front/Front.sh
@@ -1,1 +1,25 @@
+### Nginx installation
+apt install nginx -y;
+sleep 3;
+apt install prometheus -y;
+
+### Site
+wget https://raw.githubusercontent.com/Vladimir-Otus/work/refs/heads/main/Front/sites-available;
+cp nginx-sites-available /etc/nginx/sites-available/default;
+
+###  nginx restart
+systemctl restart nginx;
+
+sleep 5;
+
+###Filebeat installation
+scp -r alex@192.168.8.134:/home/vt/Distr/filebeat_8.9.1_amd64-224190-bc3f59.deb /home/vt/
+
+dpkg -i filebeat_8.9.1_amd64-224190-bc3f59.deb
+
+### filebeat configuration
+wget https://raw.githubusercontent.com/Vladimir-Otus/work/refs/heads/main/Front/filebeat-Front.yml
+cp filebeat-Front.yml /etc/filebeat/filebeat.yml
+systemctl restart filebeat
+systemctl enable filebeat
 
